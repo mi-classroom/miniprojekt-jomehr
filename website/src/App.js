8e3acb23bc15d8a857d1be2be98d37bb804f56ab
jan <janmehr93@gmx.de>
2020-09-25 18:24:19 +0200

    init react website

---
+++ website/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">WebDev Frontend</header>
+      <div className="App-main">awdas</div>
+      <footer className="App-footer">made by <a href="https://github.com/jomehr">Jan Mehr</a></footer>
+    </div>
+  );
+}
+
+export default App;
