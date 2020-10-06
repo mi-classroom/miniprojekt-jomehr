47c2aa21a1e172adce242f8007f3aef0f6384077
jan <janmehr93@gmx.de>
2020-10-06 15:41:31 +0200

    Updates

---
+++ precache-manifest.41e69e454a4b3112a71f79b588d779be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190b8c9b8542d140dc578ddbe6b56282",
+    "url": "/miniprojekt-jomehr/index.html"
+  },
+  {
+    "revision": "e3b7684ace0a01e2af02",
+    "url": "/miniprojekt-jomehr/static/css/main.b6b6c9f8.chunk.css"
+  },
+  {
+    "revision": "6978ef4003fa8e22579d",
+    "url": "/miniprojekt-jomehr/static/js/2.6d8a9a17.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/miniprojekt-jomehr/static/js/2.6d8a9a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3b7684ace0a01e2af02",
+    "url": "/miniprojekt-jomehr/static/js/main.efa6a3ce.chunk.js"
+  },
+  {
+    "revision": "b68178e240dd2f33f3dc",
+    "url": "/miniprojekt-jomehr/static/js/runtime-main.6684d617.js"
+  }
+]);
